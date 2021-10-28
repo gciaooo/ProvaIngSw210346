9f5604c5dfe58799e0c64f4c536b9bce82e961d6
gciaooo <gciaooo@gmail.com>
2021-10-28 10:49:19 +0200

    Completata classe MyListUtil

--- ProvaIngSw210346/src/main/java/it/unical/provaIngSw/MyListUtil.java
+++ ProvaIngSw210346/src/main/java/it/unical/provaIngSw/MyListUtil.java
@@ -1,19 +1,22 @@
 package it.unical.provaIngSw;
-import java.util.Comparator;
 import java.util.List;
 
 public class MyListUtil {
-	
-	private Comparator<Integer> decrescente(int i1, int i2) {
-		
-	}
 	
 	public List<Integer> sort(List<Integer> list, int order) { 
 		if (order == 0) {
 			list.sort(null);
 		}
-		else if (order == 1){
-			list.sort(decrescente(0, 0));
+		else if (order == 1) {
+			for (int i = 0; i < list.size(); i++) {
+				for (int j = 0; j < list.size(); j++) {
+					if (list.get(i) > list.get(j)) {
+						int temp = list.get(i);
+						list.set(i, list.get(j));
+						list.set(j, temp);
+					}
+				}
+			}
 		}
 		return list;
      }
